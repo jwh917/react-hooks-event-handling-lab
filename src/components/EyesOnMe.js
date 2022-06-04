de92b2cf3d7d74b3072a4ef982f041701f75a3b5
Julien Hnery <jwh_917@yahoo.com>
2022-06-04 12:29:02 -0400

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  
+  function gettingFocus(){
+    return console.log("Good!")
+  }
+
+  function gettingBlurry(){
+    return console.log("Hey! Eyes on me!")
+  }
+
+  return (
+    <button onFocus={gettingFocus} onBlur={gettingBlurry}>Eyes on me</button>
+  );
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
 // Code Keypad Component Here
+import React from "react";
+
+function Keypad() {
+
+  function passwordEnter(){
+    return console.log("Entering password...")
+  }
+
+  return (
+    <input type="password" onChange={passwordEnter}></input>
+  );
+}
+
+export default Keypad;
